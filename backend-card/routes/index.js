fd47c1bf36d9c73afe6e954940bd639612eccb00
Uzair <uzairimtiaz44@gmail.com>
2021-02-09 18:33:38 +0500

    changes

--- backend-card/routes/index.js
+++ backend-card/routes/index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/json', function(req, res, next) {
   res.status(200).json({
     "Filename":"CB_BusCard_TGoodwin.pdf", "Name":"Tracey Goodwin", "Mobile":"+61 417 467 631",
     "Email":"traceygoodwin66@gmail.com",
