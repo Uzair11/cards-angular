973d5ca16d50789020b75dedac1bc388b357e4b3
Uzair <uzairimtiaz44@gmail.com>
2021-03-03 19:14:29 +0500

    Aligned thing in UI

--- backend/app.js
+++ backend/app.js
@@ -11,13 +11,49 @@ app.use(cors());
 app.get("/generate-statement-report", async (req, res) => {
     try {
         const {statement, token, apiUrl} = req.query;
-        const response = await axios.get(`${apiUrl}/reports/statement/${statement}`, {
-            headers: {
-                AccessToken: token
-        }
-        });
+        // const response = await axios.get(`${apiUrl}/reports/statement/${statement}`, {
+        //     headers: {
+        //         AccessToken: token
+        // }
+        // });
 
-        const result=response.data;
+        const result={
+            Filename: 'CB_Stmt_TGoodwin_16Jun21.pdf',
+            Name: 'Tracey Goodwin',
+            DateRange: '16 Jun 2021 – 13 Jul 2021',
+            StatementDetail: [
+            {
+            Date: '16-Jun-2021',
+            Description: 'Credit Card Payment R’cvd',
+            Points: '-',
+            PointsBal: '-',
+            TransAmount: '$79.00',
+            CashBal: '$79.00'
+            },
+            {
+            Date: '16-Jun-2021',
+            Description: 'Weekly Membership',
+            Points: '-',
+            PointsBal: '-',
+            TransAmount: '-$19.75',
+            CashBal: '$59.25'
+            },
+            {
+            Date: '22-Jun-2021',
+            Description: 'Loyalty Reward Cash Bonus',
+            Points: '-',
+            PointsBal: '-',
+            TransAmount: '$11.85',
+            CashBal: '$71.10'
+            }
+            ],
+            CircleCount1: '5',
+            CircleCount2_12: '128',
+            CoFDetail: [
+            { Name: 'Tracey Parkins', Joined: '16 Jun 21', Circle: '01' },
+            { Name: 'Rissi Taylor', Joined: '18 Jun 21', Circle: '03' }
+            ]
+            };
         const data = await ejs.renderFile(path.join(__dirname, './views/', "statement-report-template.ejs"), {
             result
         });
